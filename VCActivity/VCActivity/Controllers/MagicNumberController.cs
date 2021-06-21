3370d725349a139432d510bfab82c648f3cba427
Kevin Niu <kevin.h.niu@gmail.com>
2021-06-21 12:52:29 -0500

    Everything

---
+++ VCActivity/VCActivity/Controllers/MagicNumberController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VCActivity.Controllers
+{
+    public class MagicNumberController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            return View(viewName: "Index", model: "Please enter your two numbers.");
+        }
+        [HttpPost]
+
+        public async Task<IActionResult> MagicNumberGenerator(int inputNumber1, int inputNumber2)
+        {
+            Random ran = new Random();
+            if(inputNumber1 >= inputNumber2)
+            {
+                return View(viewName: "Index", model: ran.Next(inputNumber2, inputNumber1));
+            }
+            else
+            {
+                return View(viewName: "Index", model: ran.Next(inputNumber1, inputNumber2));
+            }
+            
+        }
+
+    }
+}
---
+++ VCActivity/VCActivity/Controllers/StringActivityController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VCActivity.Controllers
+{
+    public class StringActivityController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            return View(viewName: "Index", model: "Please enter a string.");
+        }
+        [HttpPost]
+        public async Task<IActionResult> SayHello(string inputString)
+        {
+            return View(viewName: "Index", model: "Hello " + inputString);
+        }
+
+        public async Task<IActionResult> StringLength(string inputString)
+        {
+            return View(viewName: "Index", model: "Input length:" + inputString.Length);
+        }
+
+    }
+}
