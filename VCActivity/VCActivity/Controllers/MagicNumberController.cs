81cbbb29fad413a9161fb1b9bf4b6e0f95e82c37
SumanthKota <sumanthkota684@gmail.com>
2021-06-21 12:42:49 -0500

    Square Number and Magic Number Acitvity

---
+++ VCActivity/VCActivity/Controllers/MagicNumberController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace VCActivity.Controllers
+{
+    public class MagicNumberController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            return View(viewName: "Index", model: "Please enter an integer");
+        }
+        public async Task<IActionResult> IndexOne()
+        {
+            return View(viewName: "Index", model: "Please enter an integer");
+        }
+        public async Task<IActionResult> SayMagicNumber(int inputNumberOne, int inputNumberTwo)
+        {
+            Random rnd = new Random();
+            return View(viewName: "Index", model: "Magic Number is" + rnd.Next(inputNumberOne, inputNumberTwo);
+        }
+    }
+}
---
+++ VCActivity/VCActivity/Controllers/StringActivityController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VCActivity.Controllers
+{
+    public class StringActivityController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            return View(viewName: "Index", model: "Please enter a string.");
+
+        }
+        [HttpPost]
+        public async Task<IActionResult> SayHello(string inputString)
+        {
+            return View(viewName: "Index", model: "Hello " + inputString + " Length of String" + inputString.Length);
+        }
+        public async Task<IActionResult> Input(int inputRating)
+        {
+            return View(viewName: "Index", model: "Please enter on a scale from 1-10 on how handsome you think you are");
+        }
+        public async Task<IActionResult> SayHandsome(int inputRating)
+        {
+
+            if (inputRating > 1 && inputRating < 5)
+            {
+                return View(model: "Good luck getting a girlfriend");
+            }
+            else if (inputRating > 6 && inputRating < 8)
+            {
+                return View(model: "Decent");
+            }
+            else
+            {
+                return View(model: "Your level of handsomeness is good but not as good as Sumanth Kota");
+            }
+        }
+    }
+}
