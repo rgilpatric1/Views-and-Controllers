109212005649410127ba73706a9934b9a0631754
Benjamin Kim <22kimb@elmbrookstudenst.org>
2021-06-21 13:56:13 -0500

    Ben's Work 3

---
+++ VCActivity/VCActivity/Controllers/DatesController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VCActivity.Controllers
+{
+    public class DatesController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            return View(viewName: "Index", model: DateTime.Now.Year);
+        }
+        [HttpPost]
+        public async Task<IActionResult> DateMagic(DateTime inputBirthday)
+        {
+            int birthYear = inputBirthday.Year;
+            int yearsOld = DateTime.Now.Year - birthYear;
+            int numLeapYears = yearsOld / 4;
+            if(yearsOld % 4 == 1)
+            {
+                numLeapYears++;
+            }
+            else if(yearsOld % 4 == 2)
+            {
+                numLeapYears++;
+            }
+            else if(yearsOld % 4 == 3)
+            {
+                numLeapYears++;
+            }
+            return View(viewName: "Index", model: numLeapYears);
+        }
+    }
+}
