a47d4b49ee87d7cd40abe746bfa9f204f0169099
Mitchell <krasma423@gmail.com>
2021-06-21 12:59:10 -0500

    Mitchell 3

---
+++ VCActivity/VCActivity/Controllers/DatesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VCActivity.Controllers
+{
+    public class DatesController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            return View(viewName: "Index", model: DateTime.Now);
+        }
+        [HttpPost]
+        public async Task<IActionResult> DateMagic(DateTime inputBirthday)
+        {
+            return View(viewName: "Index", model: inputBirthday);
+        }
+    }
+}
