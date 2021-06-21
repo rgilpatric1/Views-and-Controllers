4a6b9a1f9da780bd41e291fcaf6896d5b44e0b49
Mitchell <krasma423@gmail.com>
2021-06-21 14:26:54 -0500

    Activity 7

---
+++ VCActivity/VCActivity/Controllers/StringAndMathMethodsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VCActivity.Controllers
+{
+    public class StringAndMathMethodsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SayAbsoluteValue(string lastName, int age)
+        {
+            return View(viewName: "Index", model: "The absolute value of the length of your last name subtracted by your age is: " + (Math.Abs(lastName.Length - age)));
+        }
+    }
+}
