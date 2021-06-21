46e0c1f5f1b3560b5094e5b9593d45baeb0c5dbd
Mitchell <krasma423@gmail.com>
2021-06-21 12:43:08 -0500

    First 2 Projects and Most of 3rd Project

---
+++ VCActivity/VCActivity/Controllers/MagicNumberController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VCActivity.Controllers
+{
+    public class MagicNumberController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SayMagicNumber(int inputNumber1, int inputNumber2)
+        {
+            return View(viewName: "Index", model: ((new Random()).Next(inputNumber1, inputNumber2+1)));
+        }
+    }
+}
