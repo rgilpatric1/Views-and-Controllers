d2687e0ad31a19df8c1f10c62945baa3303479d0
Mitchell <krasma423@gmail.com>
2021-06-21 14:11:53 -0500

    Activity 6

---
+++ VCActivity/VCActivity/Controllers/FirstAndLastNameController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VCActivity.Controllers
+{
+    public class FirstAndLastNameController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SayName(string firstName, string lastName)
+        {
+            return View(viewName: "Index", model: "Your name is: " + firstName + " " + lastName);
+        }
+    }
+}
